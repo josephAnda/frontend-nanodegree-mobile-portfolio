9e3bc7efb4fa79f71ffd4dc9f649029b1a45a272
Joseph Anda <orenmurasaki@gmail.com>
2015-06-23 13:11:15 -0700

    optimized pizza sizes loop

--- views/js/main.js
+++ views/js/main.js
@@ -449,8 +449,9 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  //  Stored # of pizza containers in a local variable 'max'
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    for (var i = 0, max = document.querySelectorAll(".randomPizzaContainer").length; i < max; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
